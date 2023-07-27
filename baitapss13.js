f4ba6a0a017822cca0d49b561ec25a346e53576b
anagrippine <137408340+anagrippine@users.noreply.github.com>
2023-07-27 13:48:14 +0900

    Session 13: Bài tập Function

---
+++ baitapss13.js
@@ -0,0 +1,48 @@
+//Bài 1: Viết hàm cho người dùng nhập 2 số. Tính tổng 2 số đó
+// function sum(a, b) {
+//     var a = Number(prompt("Nhập a: "));
+//     var b = Number(prompt("Nhập b: "));
+//     return console.log(a + b);
+// }
+// sum();
+
+
+//Bài 2: Viết một hàm nhận một chuỗi làm đối số và trả về độ dài của chuỗi.
+
+// function x(str) {
+//     return console.log(str.toString().length);
+// }
+// x(258782);
+// x(dddd);
+
+//Bài 3: Viết một hàm nhận một chuỗi làm đối số và trả về trả về mảng mới chứa các chuỗi viết hoa.
+// let inputArr = ['2ndj', '2fdj', '4nsj', '5ndj'];
+// let newArr = []
+// function rescr();
+// for (let i = 0; i < inputArr.length; i++) {
+//     newArr.push(inputArr[i].toUpperCase());
+//     return console.log(newArr);
+// }
+// rescr()
+
+
+//Bài 4: Viết một hàm nhận một chuỗi làm đối số và trả về chuỗi mới với các từ đảo ngược thứ tự.
+// let str = "abcdef";
+// let arr = str.split('');
+// let newStr = "";
+// function reverse(str)
+// for (let i = arr.length - 1; i >= 0; i--) {
+//     newStr += arr[i];
+//     return console.log(newStr.join(''))
+// }
+// reverse("abcdef");
+
+//Bài 5: Viết một hàm nhận một mảng các số làm đối số và trả về mảng mới chứa các số đã sắp xếp theo thứ tự tăng dần.
+// const array = [1, 2, 3, 4, 5]
+// function sapxepTangDan(a, b) {
+//     return a - b;
+// }
+// array.sort(sapxepTangDan);
+// for (i = 0; i < array.length; i++) {
+//     console.log(array[i])
+// }
